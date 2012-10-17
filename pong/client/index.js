013b5ffd74caf65f60a4f1dfd89e5609e341b7a7
José Pedro Dias <jose.pedro.dias@gmail.com>
2012-10-18 00:38:54 +0100

    ...

--- pong/client/index.js
+++ pong/client/index.js
@@ -3,28 +3,23 @@
     'use strict';
     
     /*jshint browser:true, eqeqeq:true, undef:true, curly:true, laxbreak:true, forin:true, smarttabs:true */
-    /*global stage:false, console:false, alert:false */
+    /*global stage:false, console:false, CS:false */
 
 
 
-    var cvsEl, ctx;
+    var scr = CS.createScreen({
+        scale:      1,
+        resize:     true,
+        handleKeys: true
+    });
+
+    document.body.appendChild(scr.el);
+
+    window.scr = scr;   // make scr available
+
 
     var onGameStarting = function() {
         var cfg = stage._cfg;
-        var dims = [100, 75];
-        var s = 10;
-        cvsEl = document.createElement('canvas');
-        cvsEl.setAttribute('width',  dims[0]);
-        cvsEl.setAttribute('height', dims[1]);
-        cvsEl.className = 'nnInterp';
-        cvsEl.style.width  = dims[0] * s + 'px';
-        cvsEl.style.height = dims[1] * s + 'px';
-        cvsEl.style.marginLeft = -dims[1]/2 * s + 'px';
-        cvsEl.style.marginTop  = -dims[1]/2 * s + 'px';
-        document.body.appendChild(cvsEl);
-        ctx = cvsEl.getContext('2d');
-        ctx.fillStyle = '#FFF';
-        ctx.fillRect(0, 0, dims[0], dims[1]);
 
         stage.subscribe('message',  function(msg) {
         });
@@ -44,6 +39,8 @@
         return stage.send('play', dir);
     });
     
-    stage.syncSession(onGameStarting);
+    stage.init(function(session, cfg) {
+        stage.logIn(session, onGameStarting);
+    });
 
 })();
