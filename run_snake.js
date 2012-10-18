6799690b8c34649fbb8fd40350bac70efda59817
José Pedro Dias <jose.pedro.dias@gmail.com>
2012-10-18 18:20:42 +0100

    ...

--- run_snake.js
+++ run_snake.js
@@ -3,10 +3,10 @@ var st = require('./stage-core');
 new st({
     port:     9000,
     rootDir:  __dirname + '/snake',
-    fps:      20,
+    fps:      15,
 
     mapDims:        [60, 40],
     pixelMult:      12,
-    increaseEveryN: 5,
+    increaseEveryN: 10,
     startLength:    4
 });
--- snake/client/index.js
+++ snake/client/index.js
@@ -39,7 +39,7 @@
         });
 
         stage.subscribe('alert', function(o) {
-            alert(o);
+            console.log(o);
         });
     };
 
