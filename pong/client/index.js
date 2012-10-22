e4f9c54e5447e5ede2ec0f09ddc2aa5fb7485c77
José Pedro Dias <jose.pedro.dias@gmail.com>
2012-10-22 11:27:45 +0100

    rayCast improvements

--- pong/client/index.js
+++ pong/client/index.js
@@ -40,6 +40,19 @@
         ctx.stroke();
     };
 
+    var polygonDraw = function(ctx) {
+        ctx.fillStyle = this.color;
+
+        ctx.beginPath();
+        var p = this.points;
+        for (var i = 0, f = p.length; i < f; ++i) {
+            if (i === 0) { ctx.moveTo(p[i]); }
+            else {         ctx.lineTo(p[i]); }
+        }
+        ctx.lineTo(p[0]);
+        ctx.fill();
+    };
+
     scr.shapes.push({
         pos:    p1,
         r:      r,
@@ -54,39 +67,81 @@
         draw:   circleDraw
     });
 
-    for (var i = 0; i < 360; i += 4) {
 
-        var clr = '#0F0';
 
-        var c0 = p1;
-        var c1 = p2;
+    var rayCast = function(c0, c1, r, i0, di, ii) {
+        var fs = [];    // from (Number[])
+        var ts = [];    // to   (Number[])
+        var vs = [];    // hit? (boolean)
 
-        var p = [c0[0], c0[1]];
-        var q = [c0[0], c0[1]];
+        for (var i = i0 - di/2; i <= i0+di/2; i += ii) {
+            var p = [c0[0], c0[1]];
+            var q = [c0[0], c0[1]];
 
-        var t = rayCircleIntersection(p, q, c1, r);    //L0, L1, C, R
-        if (t) {
-            console.log(t);
-            q   = t;
-            clr = '#F00';
+            var a = i * Math.PI/180;
+            var dx = Math.cos(a);
+            var dy = Math.sin(a);
+            p[0] += dx * r;
+            p[1] += dy * r;
+            q[0] += dx * (r + 300);
+            q[1] += dy * (r + 300);
+
+            var t = rayCircleIntersection(p, q, c1, r);    //L0, L1, C, R
+
+            fs.push(p);
+            if (t) {
+                ts.push(t);
+                vs.push(true);
+            }
+            else {
+                ts.push(q);
+                vs.push(false);
+            }
         }
 
-        var a = i * 180/Math.PI;
-        var dx = Math.cos(a);
-        var dy = Math.sin(a);
-        p[0] += dx * r;
-        p[1] += dy * r;
-        q[0] += dx * (r + 300);
-        q[1] += dy * (r + 300);
+        return {
+            from: fs,
+            to:   ts,
+            hit:  vs
+        };
+    };
 
-        scr.shapes.push({
-            pos:    p,
-            pos2:   q,
-            color:  '#0F0',
-            draw:   lineDraw
-        });
-    }
+    (function() {
+        var o = rayCast(p1, p2, r, 45, 90, 2.5);
+        var i, f;
 
+        if (true) {
+            // ray lines
+            for (i = 0, f = o.from.length; i < f; ++i) {
+                scr.shapes.push({
+                    pos:    o.from[i],
+                    pos2:   o.to[i],
+                    color:  o.hit[i] ? '#F00' : '#0F0',
+                    draw:   lineDraw
+                });
+            }
+        }
+        else {
+            // ray area TODO
+            f = o.from.length;
+            var ps = [];
+
+            for (i = 0; i < f; ++i) {
+                ps.push(o.from[i]);
+            }
+
+            for (i = 0; i < f; ++i) {
+                ps.push(o.from[f - i - 1]);
+            }
+
+            scr.shapes.push({
+                points: ps,
+                color:  '#00F',
+                draw:   polygonDraw
+            });
+        }
+    })();
+    
     scr.onFrame();
 
     
